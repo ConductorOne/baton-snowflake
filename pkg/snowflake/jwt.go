99532616e698c70240f91c04492793a5a0b2020e
mchavez <miguel_chavez_m@hotmail.com>
2024-07-19 18:55:17 -0600

    Generate Fingerprint and Create JWT

--- pkg/snowflake/jwt.go
+++ pkg/snowflake/jwt.go
@@ -2,8 +2,11 @@ package snowflake
 
 import (
 	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -42,6 +45,55 @@ func (c *JWTConfig) GenerateBearerToken() (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func (c *JWTConfig) GenerateBearerTokenRsaKey(keyPath string) (string, error) {
+	// Set up Snowflake account and username in uppercase
+	account := c.AccountIdentifier
+	user := c.UserIdentifier
+	qualifiedUsername := fmt.Sprintf("%s.%s", strings.ToUpper(account), strings.ToUpper(user))
+
+	// Use the fingerprint obtained from the 'DESCRIBE USER' command in Snowflake
+	fingerprintFromDescribeUser := c.PublicKeyFingerPrint
+
+	// Get current time in UTC and set JWT lifetime to 59 minutes
+	now := time.Now().UTC()
+	lifetime := time.Minute * 59
+
+	// Construct JWT payload with issuer, subject, issue time, and expiration time
+	payload := jwt.MapClaims{
+		"iss": fmt.Sprintf("%s.%s", qualifiedUsername, fingerprintFromDescribeUser),
+		"sub": qualifiedUsername,
+		"iat": now.Unix(),
+		"exp": now.Add(lifetime).Unix(),
+	}
+
+	// Specify the encoding algorithm for JWT
+	encodingAlgorithm := jwt.SigningMethodRS256
+
+	// Read private key from file and encode payload into JWT
+	pemData, err := os.ReadFile(keyPath)
+	if err != nil {
+		return "", err
+	}
+
+	block, _ := pem.Decode(pemData)
+	if block == nil {
+		return "", fmt.Errorf("failed to parse PEM block containing the private key")
+	}
+
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(encodingAlgorithm, payload)
+	signedToken, err := token.SignedString(privateKey)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
 }
 
 func ReadPrivateKey(path string) (*rsa.PrivateKey, error) {
